80da93593287dd9b7e4ad71c6303ce5037c70a4b
Paul Padamadan <paul@zephony.com>
2024-04-03 17:31:53 +0530

    Add second page for creat shipment

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
-import { Box, Icon, Text } from "@chakra-ui/react";
+import { Box, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, Icon, Spacer, Tag, TagLabel, Text } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
 
-import { KeyValueProps, RatingsProps } from "../interfaces";
+import { CarrierCardProps, FilterBoxProps, KeyValueProps, RatingsProps } from "../interfaces";
 
 const Ratings: React.FC<RatingsProps> = ({rating, maxRating = 5}) => {
     return <Box>
@@ -20,7 +20,55 @@ const KeyValueBox: React.FC<KeyValueProps> = ({ title, value}) => {
     </Box>
 }
 
+const FilterBox: React.FC<FilterBoxProps> = ({ title, children }) => {
+    return <Box className='filter-box' p='20px' bg='white' borderRadius='10px'>
+        <Text size='xs' fontWeight='bold' mb='15px'>{title}</Text>
+        <Box>
+            {children}
+        </Box>
+    </Box>
+}
+
+const CarrierCard: React.FC<CarrierCardProps> = ({ title }) => {
+    return <Card maxW='430px' borderRadius='20px'>
+        <CardHeader>
+            <Heading as='h1' size='md'>{title}</Heading> 
+        </CardHeader>
+        <CardBody>
+            <Flex gap='3'>
+                <KeyValueBox title='On-time delivery %' value='90%' />
+                <KeyValueBox title='On-time delivery %' value='90%' />
+                <KeyValueBox title='On-time delivery %' value='90%' />
+            </Flex>
+        </CardBody>
+            <Divider mt='20px' borderColor='gray.300' />
+        <CardFooter>
+            <Flex w='100%'>
+                <Tag
+                    size='lg' key='sm'
+                    borderRadius='full'
+                    variant='solid'
+                    bg='#cdfad9'
+                    color='#29945a'
+                >
+                    <TagLabel>Available</TagLabel>
+                </Tag> 
+                <Spacer />
+                <Flex align='center' gap='2'>
+                    <Icon
+                        as={StarIcon}
+                        color='yellow.400'
+                    />
+                    <Text>4.8</Text>
+                </Flex>
+            </Flex>
+        </CardFooter>
+    </Card>
+}
+
 export {
     Ratings,
+    FilterBox,
     KeyValueBox,
+    CarrierCard
 }
--- src/shipments/createShipment.tsx
+++ src/shipments/createShipment.tsx
@@ -18,15 +18,11 @@ import {
     Radio,
     CheckboxGroup,
     Checkbox,
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Tag,
-    TagLabel,
-    TagRightIcon,
-    Icon,
+    Button,
+    IconButton,
     Divider,
+    AbsoluteCenter,
+    Icon,
     // SliderMark,
     // Heading,
 
@@ -40,12 +36,16 @@ import {
 
 import Sidebar from "../components/sidebar";
 import Header from "../components/header";
-import React from "react";
-import { CarrierCardProps, FilterBoxProps } from "../interfaces";
-import { KeyValueBox, Ratings } from "../components";
-import { StarIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
+import { CarrierCard, FilterBox, Ratings } from "../components";
+
+import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+// import { PiTruckThin } from "react-icons/pi";
+// import { FaTruck } from "react-icons/fa6";
 
 const CreateShipmentPage: React.FC = () => {
+    const [page, setPage] = useState<number>(2);
+
     return <div className='create-shipment-page'>
         <Grid
             templateAreas={`
@@ -62,120 +62,126 @@ const CreateShipmentPage: React.FC = () => {
                 <Sidebar />
             </GridItem>
             <GridItem area='main' pl='5'>
-                <Flex className='carriers-filters' direction='column' h='100%'>
-                    <Box pl={5} pt={5}>
-                        <Heading as='h1' size='md'>Add Shipment</Heading>
-                    </Box>
-                    <HStack pt='5'>
-                        <Input placeholder='Pickup' maxW='250px' bg='white' />
-                        <Input placeholder='Destination' maxW='250px' bg='white' />
-                        <Input placeholder='Pickup Date' type='date' maxW='250px' bg='white' />
-                    </HStack>
-                    <Flex className='carrier-listing-filters' flexGrow='1' gap='3' mt='15px' mr='15px' mb='15px'>
-                        <Flex className='filters' direction='column' flexGrow='1' w='15%' borderRadius='10px'>
-                            <FilterBox title='Cost'>
-                                <Slider aria-label='slider-ex-1' defaultValue={30}>
-                                    <SliderTrack>
-                                        <SliderFilledTrack />
-                                    </SliderTrack>
-                                    <SliderThumb />
-                                </Slider>
-                            </FilterBox>
-                            <Spacer />
-                            <FilterBox title='Carrier Rating'>
-                                <Ratings rating={0} />
-                            </FilterBox>
-                            <Spacer />
-                            <FilterBox title='On-time Delivery %'>
-                                <Stack>
-                                    <Radio size='md' name='1' colorScheme='blue'>
-                                        Radio
-                                    </Radio>
-                                    <Radio size='md' name='1' colorScheme='blue'>
-                                        Radio
-                                    </Radio>
-                                    <Radio size='md' name='1' colorScheme='blue' defaultChecked>
-                                        Radio
-                                    </Radio>
-                                    <Radio size='md' name='1' colorScheme='blue'>
-                                        Radio
-                                    </Radio>
-                                    <Radio size='md' name='1' colorScheme='blue'>
-                                        Radio
-                                    </Radio>
-                                </Stack>
-                            </FilterBox>
-                            <Spacer />
-                            <FilterBox title='Requirements'>
-                                <CheckboxGroup colorScheme='blue' defaultValue={['naruto', 'kakashi']}>
-                                    <Stack spacing={[1, 5]}>
-                                        <Checkbox value='naruto'>Naruto</Checkbox>
-                                        <Checkbox value='sasuke'>Sasuke</Checkbox>
-                                        <Checkbox value='kakashi'>Kakashi</Checkbox>
+                {page == 1 && <>
+                    <Flex className='carriers-filters' direction='column' h='100%'>
+                        <Box pl={5} pt={5}>
+                            <Heading as='h1' size='md'>Add Shipment</Heading>
+                        </Box>
+                        <HStack pt='5'>
+                            <Input placeholder='Pickup' maxW='250px' bg='white' />
+                            <Input placeholder='Destination' maxW='250px' bg='white' />
+                            <Input placeholder='Pickup Date' type='date' maxW='250px' bg='white' />
+                        </HStack>
+                        <Flex className='carrier-listing-filters' flexGrow='1' gap='3' mt='15px' mr='15px' mb='15px'>
+                            <Flex className='filters' direction='column' flexGrow='1' w='15%' borderRadius='10px'>
+                                <FilterBox title='Cost'>
+                                    <Slider aria-label='slider-ex-1' defaultValue={30}>
+                                        <SliderTrack>
+                                            <SliderFilledTrack />
+                                        </SliderTrack>
+                                        <SliderThumb />
+                                    </Slider>
+                                </FilterBox>
+                                <Spacer />
+                                <FilterBox title='Carrier Rating'>
+                                    <Ratings rating={0} />
+                                </FilterBox>
+                                <Spacer />
+                                <FilterBox title='On-time Delivery %'>
+                                    <Stack>
+                                        <Radio size='md' name='1' colorScheme='blue'>
+                                            Radio
+                                        </Radio>
+                                        <Radio size='md' name='1' colorScheme='blue'>
+                                            Radio
+                                        </Radio>
+                                        <Radio size='md' name='1' colorScheme='blue' defaultChecked>
+                                            Radio
+                                        </Radio>
+                                        <Radio size='md' name='1' colorScheme='blue'>
+                                            Radio
+                                        </Radio>
+                                        <Radio size='md' name='1' colorScheme='blue'>
+                                            Radio
+                                        </Radio>
                                     </Stack>
-                                </CheckboxGroup>
-                            </FilterBox>
+                                </FilterBox>
+                                <Spacer />
+                                <FilterBox title='Requirements'>
+                                    <CheckboxGroup colorScheme='blue' defaultValue={['naruto', 'kakashi']}>
+                                        <Stack spacing={[1, 5]}>
+                                            <Checkbox value='naruto'>Naruto</Checkbox>
+                                            <Checkbox value='sasuke'>Sasuke</Checkbox>
+                                            <Checkbox value='kakashi'>Kakashi</Checkbox>
+                                        </Stack>
+                                    </CheckboxGroup>
+                                </FilterBox>
+                                <Box mt='20px'>
+                                    <Button isDisabled={true} w='100%' colorScheme='green' size='lg'>Next</Button>
+                                </Box>
+                            </Flex>
+                            <Spacer />
+                            <Box className='carrier-results' p='45px' w='85%' bg='white' borderRadius='10px'>
+                                <Heading as='h1' size='md'>Carriers</Heading>
+                                <Flex className='carriers' gap='5' mt='30px'>
+                                    <CarrierCard title='Transit Logistics' />
+                                    <CarrierCard title='Transit Logistics' />
+                                    <CarrierCard title='Transit Logistics' />
+                                </Flex>
+                            </Box>
                         </Flex>
-                        <Spacer />
-                        <Box className='carrier-results' p='45px' w='85%' bg='white' borderRadius='10px'>
-                            <Heading as='h1' size='md'>Carriers</Heading>
-                            <Flex className='carriers' gap='5' mt='30px'>
-                               <CarrierCard title='Transit Logistics' /> 
-                               <CarrierCard title='Transit Logistics' /> 
-                               <CarrierCard title='Transit Logistics' /> 
+                    </Flex>
+                </>}
+                {page == 2 && <Box>
+                    <Heading mt='20px'>
+                        <Flex gap='4'>
+                            <IconButton icon={<ArrowBackIcon />} size='xl' aria-label='Confirmation' />
+                            <Text>Confirmation</Text>
+                        </Flex>
+                    </Heading>
+                    <Flex maxW='900px' mt='50px' bg='white' borderRadius='10px'>
+                        <Box className='shipping-details' flexGrow='3' pt='50px' pl='50px' pb='50px' borderRight='1px' borderColor='gray.100'>
+                            <Text fontSize='xl' fontWeight='bold'>Transit Logistics</Text>
+                            <Flex pt='15px' pb='15px' pr='20px' borderTop='1px' borderBottom='1px' borderRight='1px' borderColor='gray.100'>
+                                <Flex className='pickup-details' direction='column' gap='2'>
+                                    <Text color='gray.500' fontSize='2xs' fontWeight='bold'>Starting at</Text>
+                                    <Text fontWeight='bold'>Springfield</Text>
+                                    <Text color='gray.500' fontSize='xs' fontWeight='bold'>on Apr 2, 2024</Text>
+                                </Flex>
+                                <Flex position='relative' flexGrow='1' padding='10' alignItems='center'>
+                                    <Divider />
+                                    <AbsoluteCenter bg='white' px='4'>
+                                        <Icon as={ArrowForwardIcon} color='gray.400' />
+                                    </AbsoluteCenter>
+                                </Flex>
+                                <Flex className='delivery-details' direction='column' gap='2'>
+                                    <Text color='gray.500' fontSize='2xs' fontWeight='bold'>Delivery at</Text>
+                                    <Text fontWeight='bold'>Springfield</Text>
+                                    <Text color='gray.500' fontSize='xs' fontWeight='bold'>on Apr 2, 2024</Text>
+                                </Flex>
+                            </Flex>
+                            <Box>
+                                <Text>Full load container</Text>
+                            </Box>
+                        </Box>
+                        <Box className='total-confirmation' flexGrow='1' mt='auto' mb='auto' pl='15px' pr='15px'>
+                            <Flex direction='column'>
+                                <Text ml='auto' mr='auto' fontSize='xs' fontWeight='bold' color='grey.500'>Total Cost</Text>
+                                <Text ml='auto' mr='auto'fontSize='lg' fontWeight='bold'>2400</Text>
+                                <Button mt='15px' colorScheme='green'>
+                                    Confirm Booking
+                                </Button>
                             </Flex>
                         </Box>
                     </Flex>
-                </Flex>
+                </Box>}
+                {page == 3 && <>
+
+                </>}
             </GridItem>
         </Grid>
     </div>
 }
 
-const FilterBox: React.FC<FilterBoxProps> = ({ title, children }) => {
-    return <Box className='filter-box' p='20px' bg='white' borderRadius='10px'>
-        <Text size='xs' fontWeight='bold' mb='15px'>{title}</Text>
-        <Box>
-            {children}
-        </Box>
-    </Box>
-}
-
-const CarrierCard: React.FC<CarrierCardProps> = ({ title }) => {
-    return <Card maxW='430px' borderRadius='20px'>
-        <CardHeader>
-            <Heading as='h1' size='md'>{title}</Heading> 
-        </CardHeader>
-        <CardBody>
-            <Flex gap='3'>
-                <KeyValueBox title='On-time delivery %' value='90%' />
-                <KeyValueBox title='On-time delivery %' value='90%' />
-                <KeyValueBox title='On-time delivery %' value='90%' />
-            </Flex>
-        </CardBody>
-            <Divider mt='20px' borderColor='gray.300' />
-        <CardFooter>
-            <Flex w='100%'>
-                <Tag
-                    size='lg' key='sm'
-                    borderRadius='full'
-                    variant='solid'
-                    bg='#cdfad9'
-                    color='#29945a'
-                >
-                    <TagLabel>Available</TagLabel>
-                </Tag> 
-                <Spacer />
-                <Flex align='center' gap='2'>
-                    <Icon
-                        as={StarIcon}
-                        color='yellow.400'
-                    />
-                    <Text>4.8</Text>
-                </Flex>
-            </Flex>
-        </CardFooter>
-    </Card>
-}
 
 export default CreateShipmentPage;
