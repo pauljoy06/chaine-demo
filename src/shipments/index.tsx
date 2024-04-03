8263e04a7a7cc9a83dd057405d9ef3508ad6de92
Paul Padamadan <paul@zephony.com>
2024-04-04 02:41:35 +0530

    Styling fixes

--- src/shipments/index.tsx
+++ src/shipments/index.tsx
@@ -24,22 +24,22 @@ import { SearchIcon } from "@chakra-ui/icons"
 
 import Sidebar from "../components/sidebar"
 import { Link } from "react-router-dom"
+import Header from "../components/header"
 
 const ShipmentsPage: React.FC = () => {
-
-
     return <div className='shipments-page'>
         <Grid
             templateAreas={`
                 "header         header"
                 "sidebar        main"
             `}
-            gridTemplateRows={'100px   calc(100vh - 100px)'}
+            gridTemplateRows={'80px   calc(100vh - 80px)'}
             gridTemplateColumns={'180px   1fr'}
         >
-            <GridItem area={'header'} bg='orange.300'>
+            <GridItem area={'header'}>
+                <Header />
             </GridItem>
-            <GridItem area={'sidebar'} bg='red.300'>
+            <GridItem area={'sidebar'}>
                 <Sidebar />
             </GridItem>
             <GridItem area={'main'}>
@@ -47,40 +47,38 @@ const ShipmentsPage: React.FC = () => {
                     <Heading as='h1' size='md'>Shipping</Heading>
                 </Box>
                 <Flex className='page-top-row' pl={5} pt={5} pr={5}>
-                    <InputGroup size='sm' maxW='250'>
-                        <InputLeftAddon bg='white' pointerEvents='none'>
-                            <SearchIcon color='gray.300' />
-                        </InputLeftAddon>
-                        <Input placeholder='Search' borderLeft={0} />
-                    </InputGroup>
                     <Spacer />
                     {/* TODO: Add the filter for Shipments*/}
                     <Button as={Link} to='/shipments/create' colorScheme='blue' size='sm'>New Shipment</Button>
                 </Flex>
-                <TableContainer pt='20' pl='5' pr='5' pb='10'>
-                    <Table variant='simple'>
+                <TableContainer ml='7' mt='7' mr='7' bg='white' borderRadius='10px'>
+                    <Table variant='simple' bg='white'>
                         <Thead>
                             <Tr>
                                 <Th>Shipper</Th>
                                 <Th>Carrier</Th>
                                 <Th>Content</Th>
+                                <Th>Pickup Date</Th>
                             </Tr>
                         </Thead>
                         <Tbody>
                             <Tr>
-                                <Td>Test 1</Td>
-                                <Td> NAAAAAA</Td>
+                                <Td>George Manson</Td>
+                                <Td>Budget Haulers</Td>
                                 <Td>Frozen Meat</Td>
+                                <Td>05 April, 2024</Td>
                             </Tr>
                             <Tr>
-                                <Td>Test 1</Td>
-                                <Td> NAAAAAA</Td>
+                                <Td>Kevin McMahon</Td>
+                                <Td>Reliable Transport</Td>
                                 <Td>Frozen Meat, New Age Food</Td>
+                                <Td>07 April, 2024</Td>
                             </Tr>
                             <Tr>
-                                <Td>Test 1</Td>
-                                <Td> NAAAAAA</Td>
+                                <Td>John Adams</Td>
+                                <Td>Fast Track Shipping</Td>
                                 <Td>Frozen Meat</Td>
+                                <Td>11 April, 2024</Td>
                             </Tr>
                         </Tbody>
                     </Table>
